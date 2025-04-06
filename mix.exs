83fd68a27dcf7ad3923ebda3653d2d2acdb11e83
John Maxwell <jm@wearecococo.com>
2025-04-06 15:18:34 +0200

    fix: lock luerl to 1.2.3 until audit of 1.3.0 changes complete

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule Scriptengine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:lua, github: "wearecococo/lua", ref: "44de4086478c37f390eafc5a38e1cf6a713273c7"},
+      {:lua, github: "wearecococo/lua", ref: "918fbaf5a30bef606286641c2dd7b1f145579029"},
+      {:luerl, "~> 1.2.3"},
       {:uuid, "~> 1.1"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
